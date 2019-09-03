efe1767197c293000e3e513f2d9e5894e4e62c50
Otto <giotto007@gmail.com>
2019-09-03 19:03:39 +0100

    :white_check_mark: unit test update to test code coverage

--- PaymentGateway.Service.Tests/PaymentHistoryServiceTests.cs
+++ PaymentGateway.Service.Tests/PaymentHistoryServiceTests.cs
@@ -1,7 +1,18 @@
+using System;
+using Xunit;
+
 namespace PaymentGateway.Service.Tests
 {
     public class PaymentHistoryServiceTests
     {
-        
+        [Fact]
+        public void NotImplementedExceptionIsBeingThrown()
+        {
+            var service = new PaymentHistoryService();
+
+            Action act = () => service.GetPaymentHistory(new Guid());
+
+            Assert.Throws<NotImplementedException>(act);
+        }
     }
 }
