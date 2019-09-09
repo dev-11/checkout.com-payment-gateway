df0df49d84db70d51ef3cbeb906117ba0cd34d7a
Otto <otto@masterbranch.io>
2019-09-09 14:09:52 +0100

    :bug: move class into namespace

--- PaymentGateway.WebApi/Auth/HasScopeRequirement.cs
+++ PaymentGateway.WebApi/Auth/HasScopeRequirement.cs
@@ -1,14 +1,17 @@
 using System;
 using Microsoft.AspNetCore.Authorization;
 
-public class HasScopeRequirement : IAuthorizationRequirement
+namespace PaymentGateway.WebApi.Auth
 {
-    public string Issuer { get; }
-    public string Scope { get; }
-
-    public HasScopeRequirement(string scope, string issuer)
+    public class HasScopeRequirement : IAuthorizationRequirement
     {
-        Scope = scope ?? throw new ArgumentNullException(nameof(scope));
-        Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
-    }
+        public string Issuer { get; }
+        public string Scope { get; }
+
+        public HasScopeRequirement(string scope, string issuer)
+        {
+            Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+            Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
+        }
+    }    
 }
