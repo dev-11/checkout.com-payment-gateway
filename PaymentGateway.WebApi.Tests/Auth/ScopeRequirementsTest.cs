6b5bf81ae25748fc130cdce40f6d1d3b6f0fdea6
Otto <otto@masterbranch.io>
2019-09-12 22:57:04 +0100

    ✅ fixing unit test

--- PaymentGateway.WebApi.Tests/Auth/ScopeRequirementsTest.cs
+++ PaymentGateway.WebApi.Tests/Auth/ScopeRequirementsTest.cs
@@ -24,7 +24,7 @@ namespace PaymentGateway.WebApi.Tests.Auth
         {
             var hasScopeRequirement = new ScopeRequirement(testScope, testIssuer);
             
-            hasScopeRequirement.Issuer.Should().Be(testIssuer);
+            hasScopeRequirement.Issuer.Should().Be(testSope);
             hasScopeRequirement.Scope.Should().Be(testIssuer);
         }
     }
